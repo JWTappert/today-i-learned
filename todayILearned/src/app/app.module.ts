1093ba2197bfd4f89d01cdcbdf64533b3d6888ec
JWTappert <justin@theflybook.com>
2018-11-07 20:09:18 -0800

    initing basic format

--- todayILearned/src/app/app.module.ts
+++ todayILearned/src/app/app.module.ts
@@ -5,13 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
 import { AuthInterceptor } from './auth/auth.interceptor.service';
 import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav/nav.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, CommonModule, FormsModule, AmplifyAngularModule],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, NavComponent, HomeComponent],
+  imports: [BrowserModule, HttpClientModule, CommonModule, FormsModule, AppRoutingModule, AmplifyAngularModule],
   providers: [
     AmplifyService,
     {
